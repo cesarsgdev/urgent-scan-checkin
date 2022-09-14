441f360d75760e65f2178bfa747733939f90e79c
Cesar Sepulveda <cesarsg.dev@gmail.com>
2022-09-14 00:28:21 -0500

    Added controllers for all checkin routes.

---
+++ controllers/checkins.js
@@ -0,0 +1,90 @@
+const Checkins = require("../models/Checkins");
+
+const getCheckins = async (req, res) => {
+  try {
+    const checkins = await Checkins.find().populate({ path: "eventId" });
+    if (checkins) {
+      res.status(200).json({ success: true, data: checkins });
+      return;
+    }
+  } catch (e) {
+    res.status(200).json({ success: true, message: e.message });
+    return;
+  }
+};
+
+exports.getCheckins = getCheckins;
+
+const getSingleCheckin = async (req, res) => {
+  try {
+    const checkin = await Checkins.findById(req.params.id);
+    if (checkin) {
+      res.status(200).json({ success: true, data: checkin });
+      return;
+    }
+  } catch (e) {
+    res.status(200).json({ success: false, message: e.message });
+    return;
+  }
+};
+
+exports.getSingleCheckin = getSingleCheckin;
+
+const checkinGuest = async (req, res) => {
+  try {
+    const checkin = await new Checkins(req.body);
+    await checkin.save();
+    res.status(200).json({ success: true, data: checkin });
+    return;
+  } catch (e) {
+    res.status(200).json({ success: false, message: e.message });
+    return;
+  }
+};
+
+exports.checkinGuest = checkinGuest;
+
+const modifyCheckin = async (req, res) => {
+  try {
+    const checkin = await Checkins.findByIdAndUpdate(
+      { _id: req.params.id },
+      { name: req.body.name }
+    );
+
+    if (checkin) {
+      res.status(200).json({ success: true, data: checkin });
+    } else {
+      res
+        .status(200)
+        .json({
+          success: false,
+          message: `Checkin with object ${req.params.id} not found. Please try again.`,
+        });
+    }
+  } catch (e) {
+    res.status(200).json({ success: false, message: e.message });
+  }
+};
+
+exports.modifyCheckin = modifyCheckin;
+
+const deleteCheckin = async (req, res) => {
+  try {
+    const checkin = await Checkins.findByIdAndDelete(req.params.id);
+    if (checkin) {
+      res.status(200).json({ success: true, data: checkin });
+      return;
+    } else {
+      res.status(200).json({
+        success: false,
+        message: `Checkin with object ${req.params.id} not found. Please try again.`,
+      });
+      return;
+    }
+  } catch (e) {
+    res.status(200).json({ success: false, message: e.message });
+    return;
+  }
+};
+
+exports.deleteCheckin = deleteCheckin;
--- routes/checkins.js
+++ routes/checkins.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const checkins = require("../controllers/checkins");
 
 // Endpoint to get all the checkins available in all the events => GET /api/checkins
-router.get("/", async (req, res) => {});
+router.get("/", checkins.getCheckins);
 
 // Endpoint to get the info about a single checkin by :id => GET /api/checkins/:id
-router.get("/:id", async (req, res) => {});
+router.get("/:id", checkins.getSingleCheckin);
 
 // Endpoint to create a new checkin attached to a specific event :id => POST /api/checkins/
-router.post("/", async (req, res) => {});
+router.post("/", checkins.checkinGuest);
 
 // Endpoint to modify a single checkin attached to an event by checkin :id => PUT /api/checkins/:id
-router.put("/", async (req, res) => {});
+router.put("/:id", checkins.modifyCheckin);
 
 // Endpoint to delete a single checkin attached to an event by checkin :id => DELETE /api/checkins/:id
-router.delete("/", async (req, res) => {});
+router.delete("/:id", checkins.deleteCheckin);
 
 module.exports = router;
