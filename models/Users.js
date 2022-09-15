bccfe10cafb03d7633192690c6f02c93b7a79b58
Cesar Sepulveda <cesarsg.dev@gmail.com>
2022-09-15 02:00:51 -0500

    Added User model with middleware to encrypt password before it is saved to DB.

---
+++ models/Users.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
+const saltRounds = 12;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      minLength: 5,
+      maxLength: 50,
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      minLength: 6,
+      maxLength: 50,
+    },
+
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      minLength: 8,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    if (err) throw err;
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) throw err;
+      else {
+        user.password = hash;
+        next();
+      }
+    });
+  });
+});
+
+const Users = mongoose.model("users", userSchema);
+
+module.exports = Users;
