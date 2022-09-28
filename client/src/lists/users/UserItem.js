1746e24f31762d329a888aa40e059be03e85a71d
Cesar Sepulveda <cesarsg.dev@gmail.com>
2022-09-28 00:07:30 -0500

    Made UserItem component responsive.

--- client/src/lists/users/UserItem.js
+++ client/src/lists/users/UserItem.js
@@ -2,11 +2,11 @@ const UserItem = ({ user }) => {
   const { _id, name, email } = user;
   return (
     <>
-      <div className=" w-[100%] bg-white shadow-sm p-4 rounded-md flex justify-between">
-        <span className="w-[40%] flex justify-center flex-auto text-md antialiased font-bold text-aqua-100 text-lg border-r-2 border-gray-100">
+      <div className=" w-[100%] bg-white shadow-sm p-4 rounded-md flex flex-col justify-between md:flex-row">
+        <span className=" flex flex-auto text-xl antialiased font-bold text-aqua-100 md:text-lg md:w-[40%] md:border-r-2 md:border-gray-100 md:justify-center">
           {name}
         </span>
-        <span className="w-[40%] flex justify-center flex-auto text-gray-400 font-bold antialiased text-lg">
+        <span className="flex  flex-auto text-sm text-gray-400 font-bold antialiased md:text-lg md:w-[40%] md:justify-center">
           {email}
         </span>
       </div>
