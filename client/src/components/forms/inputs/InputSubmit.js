b732daef00e9217f7bef2225851dbd7ce7042816
Cesar Sepulveda <cesarsg.dev@gmail.com>
2022-10-01 23:50:38 -0500

    Added style for disabled button to Submit button component

--- client/src/components/forms/inputs/InputSubmit.js
+++ client/src/components/forms/inputs/InputSubmit.js
@@ -1,11 +1,16 @@
-const InputSubmit = ({ value = "Submit", id = "submitNewEvent" }) => {
+const InputSubmit = ({
+  value = "Submit",
+  id = "submitNewEvent",
+  disabled = false,
+}) => {
   return (
     <>
       <input
         id={id}
-        className="w-[100%] bg-aqua-100 text-white antialiased font-bold text-lg h-10 rounded-md transition duration-500 cursor-pointer hover:bg-aqua-400 mt-2"
+        className="w-[100%] bg-aqua-100 text-white antialiased font-bold text-lg h-10 rounded-md transition duration-500 cursor-pointer hover:bg-aqua-400 mt-2 disabled:bg-gray-300 disabled:text-gray-500"
         type="submit"
         value={value}
+        disabled={disabled}
       />
     </>
   );
