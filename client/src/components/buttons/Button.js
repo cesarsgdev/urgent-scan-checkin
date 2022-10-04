097d9e608661f01e0a1b90893b3af55407fe9d59
Cesar Sepulveda <cesarsg.dev@gmail.com>
2022-10-03 23:43:59 -0500

    Small change to Button component to add custom classes if needed.

--- client/src/components/buttons/Button.js
+++ client/src/components/buttons/Button.js
@@ -1,5 +1,5 @@
 const Button = ({ className, label, onClick }) => {
-  const btnClass = ["cursor-pointer px-4 py-2 rounded-md", className].join("");
+  const btnClass = ["cursor-pointer px-4 py-2 rounded-md", className].join(" ");
 
   return (
     <>
